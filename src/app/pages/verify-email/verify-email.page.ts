d1102d8f2a92e289bbb995bbe9977e1c27be14f7
Fellipe <cliente@Kovi-Fellipe.local>
2020-12-09 16:46:03 -0300

    add verify email files

--- src/app/pages/verify-email/verify-email.page.ts
+++ src/app/pages/verify-email/verify-email.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-verify-email',
@@ -10,24 +10,34 @@ import { AuthService } from '../../services/auth.service';
 })
 export class VerifyEmailPage implements OnInit {
 
-  constructor(private authService: AuthService, private alertController: AlertController) {
-  }
+  constructor(
+    private alertController: AlertController,
+    private loadingController: LoadingController,
+    private authService: AuthService
+  ) { }
 
   async presentAlert(message, header = 'Ocorreu um erro') {
     const alert = await this.alertController.create({ header, message, buttons: ['OK'] });
     await alert.present();
   }
 
+  async presentLoading() {
+    const loading = await this.loadingController.create({});
+    await loading.present();
+  }
+
   ngOnInit() {
   }
 
-  resendEmailVerification() {
-    this.authService.sendEmailVerification()
+  async resendEmailVerification() {
+    await this.presentLoading();
+
+    await this.authService.sendEmailVerification()
       .then(() => {
         return this.presentAlert('Reenviamos um e-mail com o link para confirmar seu cadastro. Se não encontrar na caixa de entrada, favor verificar no spam.', 'Verifique seu Email');
       })
-      .catch((error) => {
-        this.presentAlert(error);
-      });
+      .catch((error) => this.presentAlert(error));
+
+    await this.loadingController.dismiss();
   }
 }
