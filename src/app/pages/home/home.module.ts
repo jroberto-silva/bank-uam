ddcfe6209b8ac890e15620a7ac622bac00bffa0f
José Roberto da Silva <jroberto-silva@outlook.com>
2020-12-10 00:32:09 -0300

    Update home page to use previous separated card components, instead of duplicated content

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
