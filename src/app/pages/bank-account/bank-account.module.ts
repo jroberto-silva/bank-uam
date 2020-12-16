bdd79c8881f96ca0a65272070b442d9e065af683
Fellipe <cliente@Kovi-Fellipe.local>
2020-12-15 22:40:29 -0300

    bank account page

--- src/app/pages/bank-account/bank-account.module.ts
+++ src/app/pages/bank-account/bank-account.module.ts
@@ -5,7 +5,6 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { BankAccountPageRoutingModule } from './bank-account-routing.module';
 import { BankAccountPage } from './bank-account.page';
-import { BankAccount } from 'src/app/models/bank.account.model';
 
 @NgModule({
   imports: [
@@ -15,15 +14,4 @@ import { BankAccount } from 'src/app/models/bank.account.model';
   declarations: [BankAccountPage]
 })
 export class BankAccountPageModule {
-  public bankAccount: BankAccount;
-  public loadingBankAccount: boolean = true;
-
-  public setBankAccount(bankAccount: BankAccount) {
-    this.bankAccount = bankAccount;
-  }
-
-  public setLoadingBankAccount(loading: boolean) {
-    this.loadingBankAccount = loading;
-  }
 }
-
