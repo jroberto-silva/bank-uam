ab7ac72cdc3312953efcb9e07ddf53f99e5a4abf
JULIOREIS1 <menezes.julioc@gmail.com>
2020-12-15 23:15:06 -0300

    Apresentando nome correto do usuário na home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public authService: AuthService) {}
+  public user: User;
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user')) as User;
+  }
 }
